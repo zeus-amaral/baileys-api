cdd975f4db24a53dbe04a216aff311da39f91c89
gabrieljablonski <contact@gabrieljablonski.com>
2025-03-31 22:30:35 -0300

    chore: drop unused utility and update config to use default values

--- src/config.ts
+++ src/config.ts
@@ -1,39 +1,19 @@
 import packageInfo from "@/../package.json";
 import type { LevelWithSilentOrString } from "pino";
 
-const { NODE_ENV, PORT, LOG_LEVEL } = process.env;
-
-if (!NODE_ENV) {
-  throw new Error("NODE_ENV is required");
-}
-if (!PORT) {
-  throw new Error("PORT is required");
-}
-if (!LOG_LEVEL) {
-  throw new Error("LOG_LEVEL is required");
-}
-
-const { BAILEYS_LOG_LEVEL, BAILEYS_PRINT_QR } = process.env;
-
-if (!BAILEYS_LOG_LEVEL) {
-  throw new Error("BAILEYS_LOG_LEVEL is required");
-}
-if (!BAILEYS_PRINT_QR) {
-  throw new Error("BAILEYS_PRINT_QR is required");
-}
-
-const { REDIS_URL, REDIS_PASSWORD } = process.env;
-
-if (!REDIS_URL) {
-  throw new Error("REDIS_URL is required");
-}
-if (!REDIS_PASSWORD) {
-  throw new Error("REDIS_PASSWORD is required");
-}
+const {
+  NODE_ENV,
+  PORT,
+  LOG_LEVEL,
+  BAILEYS_LOG_LEVEL,
+  BAILEYS_PRINT_QR,
+  REDIS_URL,
+  REDIS_PASSWORD,
+} = process.env;
 
 const config = {
   packageInfo: { name: packageInfo.name, version: packageInfo.version },
-  port: Number(PORT),
+  port: PORT ? Number(PORT) : 3025,
   env: (NODE_ENV || "development") as "development" | "production",
   logLevel: (LOG_LEVEL || "info") as LevelWithSilentOrString,
   baileys: {
@@ -41,8 +21,8 @@ const config = {
     printQr: BAILEYS_PRINT_QR === "true",
   },
   redis: {
-    url: REDIS_URL,
-    password: REDIS_PASSWORD,
+    url: REDIS_URL || "redis://localhost:6379",
+    password: REDIS_PASSWORD || "",
   },
 };
 
