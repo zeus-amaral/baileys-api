03e92a8b774335ececafdc5ce7c70474bc17268e
gabrieljablonski <contact@gabrieljablonski.com>
2025-05-20 22:46:36 -0300

    fix: brazil phone number normalization

--- src/baileys/helpers/normalizeBrazilPhoneNumber.ts
+++ src/baileys/helpers/normalizeBrazilPhoneNumber.ts
@@ -7,7 +7,7 @@
  * @returns The same phone number if does not need normalizing, or the phone number with the extra digit 9 added.
  */
 export function normalizeBrazilPhoneNumber(phoneNumber: string): string {
-  const match = phoneNumber.match(/\+55(\d{2})(\d{8})/);
+  const match = phoneNumber.match(/^\+55(\d{2})(\d{8})$/);
   if (!match) {
     return phoneNumber;
   }
